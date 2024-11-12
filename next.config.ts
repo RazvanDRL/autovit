a741d8ff91adf07640220e7686b0c8c5ee8c7d09
RazvanDRL <razdir97@gmail.com>
2024-11-12 23:55:25 +0200

    permissions policy

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'Permissions-Policy',
+                        value: 'private-state-token-redemption=(), private-state-token-issuance=(), browsing-topics=()'
+                    }
+                ],
+            },
+        ]
+    },
     images: {
         remotePatterns: [
             {

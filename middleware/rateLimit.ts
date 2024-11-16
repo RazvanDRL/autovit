cc38f7b7a726fb309742c646e308d987fc267aea
RazvanDRL <razdir97@gmail.com>
2024-11-16 13:05:41 +0200

    add security features

---
+++ middleware/rateLimit.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import { Ratelimit } from '@upstash/ratelimit';
+import { Redis } from '@upstash/redis';
+
+// Create a new ratelimiter that allows 100 requests per 15 minutes
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(10, '15 m'),
+  analytics: true,
+});
+
+export async function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname.startsWith('/api/')) {
+    const ip = request.headers.get('x-forwarded-for') ?? 'anonymous'
+    const { success } = await ratelimit.limit(ip)
+    
+    if (!success) {
+      return NextResponse.json({ error: 'Too many requests' }, { status: 429 })
+    }
+    return NextResponse.next()
+  }
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: '/api/:path*',
+}
